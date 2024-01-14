05336b60aa0d934c9826f7dff7d0d2e537b4e570
wltmd1114 <wltmd5285@gmail.com>
2024-01-14 20:31:02 +0900

    feat: Remane func name adder to add

--- adder.py
+++ adder.py
@@ -1,6 +1,7 @@
-def adder(num1: int, num2: int) -> int:
+def add(num1: int, num2: int) -> int:
+	# add -> adder 이나 이미 adder라고 해서 add로 바꿈
 	return num1+num2
 
 
 if __name__=='__main__':
-	print(adder(3,4))
+	print(add(3,4))
